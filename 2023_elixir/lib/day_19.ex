87be7f9a863684f3d36be0606edbf654478c4eee
Dawn Richardson <dawner.fr@gmail.com>
2023-12-26 22:11:22 -0800

    feat(aoc-2023): Day 19: Aplenty gear classification

---
+++ 2023_elixir/lib/day_19.ex
@@ -0,0 +1,90 @@
+defmodule Day19 do
+  @moduledoc """
+  Advent of Code 30 days of challenges: Day 19 Aplenty
+  https://adventofcode.com/2023/day/19
+  """
+
+  @doc """
+    Part 1: Classify parts depending on a set of rules, counting number accepted
+  """
+  def solve(:part_1, filename) do
+    {:ok, file_contents} = File.read(filename)
+
+    {workflows, parts} =
+      file_contents
+      |> String.split("\n", trim: true)
+      |> Enum.reduce({%{}, []}, fn line, {workflows, parts} ->
+        parse_line(line, {workflows, parts})
+      end)
+
+    results = Enum.filter(parts, fn part -> process("in", part, workflows) end)
+
+    Enum.flat_map(results, fn part -> Enum.map(part, fn {_k, v} -> v end) end)
+    |> Enum.sum()
+  end
+
+  def solve(:part_2, filename) do
+  end
+
+  defp process("A", _, _), do: true
+  defp process("R", _, _), do: false
+
+  defp process(key, part, workflows) do
+    Map.get(workflows, key)
+    |> check_rules(part, workflows)
+  end
+
+  defp check_rules([{nil, nil, nil, out}], part, workflows), do: process(out, part, workflows)
+
+  defp check_rules([{p, op, val, out} | rules], part, workflows) do
+    part_val = Map.get(part, p)
+
+    if apply_rule(part_val, op, val) do
+      process(out, part, workflows)
+    else
+      check_rules(rules, part, workflows)
+    end
+  end
+
+  defp apply_rule(part_val, "<", val), do: part_val < val
+  defp apply_rule(part_val, ">", val), do: part_val > val
+
+  defp parse_line(line, {workflows, parts}) do
+    if String.at(line, 0) == "{" do
+      {workflows, [parse_part(line) | parts]}
+    else
+      {name, operations} = parse_workflow(line)
+      {Map.put(workflows, name, operations), parts}
+    end
+  end
+
+  defp parse_workflow(line) do
+    [_, name, raw_operations] = Regex.run(~r/([a-z]+){(.*)}/, line)
+
+    operations =
+      String.split(raw_operations, ",")
+      |> Enum.map(fn rules -> parse_rules(rules) end)
+
+    {name, operations}
+  end
+
+  defp parse_rules(rule) do
+    if String.contains?(rule, ":") do
+      [_, part, op, val, out] = Regex.run(~r/([xmas]{1})([<>])([0-9]+):([a-zAR]+)/, rule)
+      {part, op, String.to_integer(val), out}
+    else
+      {nil, nil, nil, rule}
+    end
+  end
+
+  defp parse_part(line) do
+    parts_regex = ~r/{x=(?<x>[0-9]+),m=(?<m>[0-9]+),a=(?<a>[0-9]+),s=(?<s>[0-9]+)/
+    parts = Regex.named_captures(parts_regex, line)
+
+    for {k, v} <- parts, into: %{}, do: {k, String.to_integer(v)}
+  end
+end
+
+filename = "#{File.cwd!()}/files/day_19_input.txt"
+result = Day19.solve(:part_1, filename)
+IO.puts("Part 1: #{result}")
---
+++ 2023_elixir/test/day_19_test.exs
@@ -0,0 +1,13 @@
+defmodule Day19Test do
+  use ExUnit.Case
+  doctest Day19
+
+  test "solve/2 part 1" do
+    assert Day19.solve(:part_1, "#{File.cwd!()}/test/files/day_19_test_input.txt") == 19114
+  end
+
+  test "solve/2 part 2" do
+    assert Day19.solve(:part_2, "#{File.cwd!()}/test/files/day_19_test_input.txt") ==
+             167_409_079_868_000
+  end
+end
